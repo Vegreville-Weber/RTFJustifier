5d0085f2482c3617406725f3e568be5c32bb047e
bruno.vegreville <bruno.vegreville@gmail.com>
2014-12-13 20:47:44 +0100

    L'algorithme de justification marche pour une largeur identique pour chaque caractère !

--- src/editeur/OptimisationAlgorithme.java
+++ src/editeur/OptimisationAlgorithme.java
@@ -2,29 +2,76 @@ package editeur;
 
 public class OptimisationAlgorithme {
 
+	private static final double INFINI = Double.MAX_VALUE;
+
 	public static void main(String[] args) {
 		String p = "aaaaaaa aaaaaaa bbbbbbbb bb ccccc aa bbb aaaaaaa bbcc cccc";
 		double[] cara = new double[500];
 		cara['a']=1;cara['b']=1;cara['c']=1;cara[' ']=1;
 		double largeurBloc = 10 ;
-		//System.out.println(niceParagraph(p,cara,largeurBloc));
-		String[] chaine = chainesdeMots(p);
-		for(int k =0;k<chaine.length;k++) System.out.println(chaine[k]);
+		System.out.println(niceParagraph(p,cara,largeurBloc));
+		/*String[] chaine = chainesdeMots(p);
+		for(int k =0;k<chaine.length;k++) System.out.println(chaine[k]);*/
 
 	}
 	
 	/**
-	 * @param paragraphe - Paragraphe à mettre en page
+	 * @param paragraphe - Paragraphe à mettre en page EN CONSIDERANT UN SEUL ESPACE ENTRE CHAQUE MOT
 	 * @param largeur - Tableau donnant pour chaque caractère (ESPACE BLANC inclus) la largeur du caractère
 	 * @param largeurBloc - Largeur du bloc où on écrit le paragraphe.
 	 * @return - Renvoie le paragraphe mis en page
 	 */
+	
+	//LARGEUR DOIT ETRE PLUS GRAND QUE LE PLUS GRAND DES MOTS DU PARAGRAPHE
 	public static String niceParagraph(String paragraphe,double[] largeur,double largeurBloc){
 		String[] chaine = chainesdeMots(paragraphe);
-		double largeurParagraphe = largeurParagraphe(chaine,largeur);
-		if(largeurParagraphe<=largeurBloc) return paragraphe;
+		int nombreDeMots = chaine.length;
+		double[][] costs = new double[nombreDeMots + 1][nombreDeMots + 1];
+		double[] costFinal = new double[nombreDeMots + 1];
+		double[][] espaces = new double[nombreDeMots + 1][nombreDeMots + 1];
+		int[] p = new int[nombreDeMots + 1];
+
+		for (int i = 1; i <= nombreDeMots; i++) {
+			espaces[i][i] = largeurBloc - chaine[i - 1].length();
+			for (int j = i + 1; j <= nombreDeMots; j++) {
+				espaces[i][j] = espaces[i][j - 1] - chaine[j - 1].length() - 1;
+			}
+		}
+
+		for (int i = 1; i <= nombreDeMots; i++) {
+			for (int j = i; j <= nombreDeMots; j++) {
+				if (espaces[i][j] < 0)
+					costs[i][j] = INFINI;
+				else if (j == nombreDeMots && espaces[i][j] >= 0)
+					costs[i][j] = 0;
+				else
+					costs[i][j] = espaces[i][j] * espaces[i][j] * espaces[i][j];
+			}
+		}
 		
-		return null;
+		costFinal[0] = 0;
+		for (int j = 1; j <= nombreDeMots; j++) {
+			costFinal[j] = INFINI;
+			for (int i = 1; i <= j; i++) {
+				if (costFinal[i-1] != INFINI && costs[i][j] != INFINI && (costFinal[i-1] + costs[i][j] < costFinal[j])) {
+					costFinal[j] = costFinal[i - 1] + costs[i][j];
+					p[j] = i;
+				}
+			}
+		}
+		String resultat = new String();
+		int pointeur = nombreDeMots;
+		while (pointeur >= 1) {
+			String temp = "\n";;
+			System.out.println(temp);
+			for (int k = p[pointeur]; k < pointeur; k++) {
+				temp+=(chaine[k-1]+" ");
+			}
+			temp+=chaine[pointeur-1];
+			resultat = temp+=resultat;
+			pointeur = p[pointeur]-1;
+		}
+		return resultat;
 	}
 	
 	/**
