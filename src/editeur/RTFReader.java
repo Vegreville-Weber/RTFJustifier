30a0c0296a6cb5080dbac978043bf358d8c1e7ba
bruno.vegreville <bruno.vegreville@gmail.com>
2014-12-15 22:19:20 +0100

    Mise à jour du Reader

--- src/editeur/RTFReader.java
+++ src/editeur/RTFReader.java
@@ -1,29 +1,45 @@
 package editeur;
-import static com.tutego.jrtf.Rtf.rtf;
-import static com.tutego.jrtf.RtfDocfmt.*;
-import static com.tutego.jrtf.RtfHeader.*;
-import static com.tutego.jrtf.RtfInfo.*;
-import static com.tutego.jrtf.RtfFields.*;
-import static com.tutego.jrtf.RtfPara.*;
-import static com.tutego.jrtf.RtfSectionFormatAndHeaderFooter.*;
-import static com.tutego.jrtf.RtfText.*;
-import static com.tutego.jrtf.RtfUnit.*;
 
-import java.awt.Font;
-import java.io.FileWriter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
-import com.tutego.jrtf.RtfDocfmt;
-import com.tutego.jrtf.RtfText;
-import com.tutego.jrtf.RtfUnit;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.rtf.RTFEditorKit;
+
 
 public class RTFReader {
 
 	public static void main(String[] args) {
-		String lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper consequat elementum. Donec ultricies cursus justo, a eleifend mauris ultrices a. Nam elementum sapien id sodales viverra. Morbi vestibulum odio ut purus placerat tristique. Duis sed rhoncus tortor. Vestibulum in semper purus. Nam eget auctor augue. Sed ut volutpat nibh, eget mollis diam. Ut tincidunt ex et dui semper mattis. Nulla non lacus iaculis justo pretium hendrerit vitae eu odio. Sed sagittis aliquam tellus quis mollis. Ut id eleifend enim. Aliquam mollis vitae eros a dignissim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer dapibus, ligula eget finibus hendrerit, metus ante aliquam quam, aliquet laoreet ligula odio at dolor. Nulla viverra, enim ut imperdiet venenatis, eros odio porttitor neque, quis aliquet mauris turpis et sem.";
-		String loremcor = OptimisationAlgorithme.niceParagraph(new Font("Liberation Serif", Font.PLAIN, 12),lorem,481.88976378);
-		System.out.println(loremcor);
-		RTFWriter.writeRTF("Liberation Serif",12,loremcor,21);
+		RTFEditorKit editor = new RTFEditorKit();
+		InputStream inputstream = null;
+		try {
+			inputstream = new FileInputStream("OneParagraph.rtf");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		DefaultStyledDocument styledDoc=new DefaultStyledDocument();
+		try {
+			editor.read(inputstream, styledDoc, 0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String str="str";
+		try {
+			str = styledDoc.getText(0, styledDoc.getLength());
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(str);
 
 	}
 }
--- src/editeur/RTFWriter.java
+++ src/editeur/RTFWriter.java
@@ -6,6 +6,7 @@ import static com.tutego.jrtf.RtfPara.p;
 import static com.tutego.jrtf.RtfText.font;
 import static com.tutego.jrtf.RtfText.fontSize;
 
+import java.awt.Font;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -15,7 +16,10 @@ import com.tutego.jrtf.RtfUnit;
 public class RTFWriter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper consequat elementum. Donec ultricies cursus justo, a eleifend mauris ultrices a. Nam elementum sapien id sodales viverra. Morbi vestibulum odio ut purus placerat tristique. Duis sed rhoncus tortor. Vestibulum in semper purus. Nam eget auctor augue. Sed ut volutpat nibh, eget mollis diam. Ut tincidunt ex et dui semper mattis. Nulla non lacus iaculis justo pretium hendrerit vitae eu odio. Sed sagittis aliquam tellus quis mollis. Ut id eleifend enim. Aliquam mollis vitae eros a dignissim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer dapibus, ligula eget finibus hendrerit, metus ante aliquam quam, aliquet laoreet ligula odio at dolor. Nulla viverra, enim ut imperdiet venenatis, eros odio porttitor neque, quis aliquet mauris turpis et sem.";
+		String loremcor = OptimisationAlgorithme.niceParagraph(new Font("Liberation Serif", Font.PLAIN, 12),lorem,481.88976378);
+		System.out.println(loremcor);
+		writeRTF("Liberation Serif",12,loremcor,21);
 
 	}
 	public static void writeRTF(String fontName,int fontSize,String paragraphe,double largeurFeuille){
